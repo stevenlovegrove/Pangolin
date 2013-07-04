f9a7603d2d9cd5e7dafd56e115a7a6630dc97b5d
Steven Lovegrove <stevenlovegrove@gmail.com>
2013-07-04 12:03:36 -0400

    Added jpeg support to image_load.cpp.

--- src/image_load.cpp
+++ src/image_load.cpp
@@ -1,8 +1,7 @@
 /* This file is part of the Pangolin Project.
  * http://github.com/stevenlovegrove/Pangolin
  *
- * Copyright (c) 2013 Steven Lovegrove,
- *                    Gabe Sibley
+ * Copyright (c) 2013 Steven Lovegrove
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
@@ -33,6 +32,10 @@
 
 #ifdef HAVE_PNG
 #include <png.h>
+#endif
+
+#ifdef HAVE_JPEG
+#include <jpeglib.h>
 #endif
 
 namespace pangolin {
@@ -85,7 +88,7 @@ ImageFileType FileType(const std::string& filename)
     if(magic_type != ImageFileTypeUnknown) {
         return magic_type;
     }    
-
+    
     // Fallback on using extension...
     const std::string ext = FileLowercaseExtention(filename);
     if( ext == ".png" ) {
@@ -128,9 +131,9 @@ TypedImage LoadTga(const std::string& filename)
     FILE *file;
     unsigned char type[4];
     unsigned char info[6];
-
+    
     file = fopen(filename.c_str(), "rb");
-
+    
     if(file) {
         fread( &type, sizeof (char), 3, file );
         fseek( file, 12, SEEK_SET );
@@ -142,14 +145,14 @@ TypedImage LoadTga(const std::string& filename)
         TypedImage img;
         img.fmt = TgaFormat(info[4], type[2], type[1]);
         img.Alloc(width, height, width*img.fmt.bpp / 8);
-            
+        
         //read in image data
         fread(img.ptr, sizeof(unsigned char), img.w * img.pitch, file);
         fclose(file);
         
         return img;
     }
-
+    
     throw std::runtime_error("Unable to load TGA file, '" + filename + "'");    
 }
 
@@ -179,42 +182,42 @@ TypedImage LoadPng(const std::string& filename)
 {
 #ifdef HAVE_PNG
     FILE *in = fopen(filename.c_str(), "rb");
-
+    
     if( in )  {
         //check the header
         int nBytes = 8;
         png_byte header[nBytes];
         fread(header, 1, nBytes, in);
         int nIsPNG = png_sig_cmp(header, 0, nBytes);
-    
+        
         if ( nIsPNG != 0 )  {
             throw std::runtime_error( filename + " is not a PNG file" );
         }
-    
+        
         //set up initial png structs
         png_structp png_ptr = png_create_read_struct( PNG_LIBPNG_VER_STRING, (png_voidp)NULL, NULL, NULL);
         if (!png_ptr) {
             throw std::runtime_error( "PNG Init error 1" );
         }
-    
+        
         png_infop info_ptr = png_create_info_struct(png_ptr);
         if (!info_ptr)  {
             png_destroy_read_struct(&png_ptr, (png_infopp)NULL, (png_infopp)NULL);
             throw std::runtime_error( "PNG Init error 2" );
         }
-    
+        
         png_infop end_info = png_create_info_struct(png_ptr);
         if (!end_info) {
             png_destroy_read_struct(&png_ptr, &info_ptr, (png_infopp)NULL);
             throw std::runtime_error( "PNG Init error 3" );
         }
-    
+        
         png_init_io(png_ptr, in);
         png_set_sig_bytes(png_ptr, nBytes);
-    
+        
         //read the file
         png_read_png(png_ptr, info_ptr, PNG_TRANSFORM_IDENTITY, NULL);
-
+        
         if( png_get_bit_depth(png_ptr, info_ptr) == 1)  {
             //Unpack bools to bytes to ease loading.
             png_set_packing(png_ptr);
@@ -222,12 +225,12 @@ TypedImage LoadPng(const std::string& filename)
             //Expand nonbool colour depths up to 8bpp
             png_set_expand_gray_1_2_4_to_8(png_ptr);
         }
-                
+        
         //Get rid of palette, by transforming it to RGB
         if(png_get_color_type(png_ptr, info_ptr) == PNG_COLOR_TYPE_PALETTE) {
             png_set_palette_to_rgb(png_ptr);
         }
-    
+        
         if( png_get_interlace_type(png_ptr,info_ptr) != PNG_INTERLACE_NONE) {
             throw std::runtime_error( "Interlace not yet supported" );
         }
@@ -235,7 +238,7 @@ TypedImage LoadPng(const std::string& filename)
         const size_t w = png_get_image_width(png_ptr,info_ptr);
         const size_t h = png_get_image_height(png_ptr,info_ptr);
         const size_t pitch = png_get_rowbytes(png_ptr, info_ptr);
-    
+        
         TypedImage img;
         img.fmt = PngFormat(png_ptr, info_ptr);
         img.Alloc( w, h, pitch );
@@ -245,21 +248,89 @@ TypedImage LoadPng(const std::string& filename)
             memcpy( img.ptr + pitch*r, rows[r], pitch );
         }
         png_destroy_read_struct(&png_ptr, &info_ptr, &end_info);
-    
+        
         fclose(in);
         return img;
     }
-
+    
     throw std::runtime_error("Unable to load PNG file, '" + filename + "'");    
-
+    
 #else
     throw std::runtime_error("PNG Support not enabled. Please rebuild Pangolin.");
 #endif
 }
 
+struct my_error_mgr
+{
+  struct jpeg_error_mgr pub;
+  jmp_buf setjmp_buffer;
+};
+
+typedef struct my_error_mgr * my_error_ptr;
+
+METHODDEF(void) my_error_exit(j_common_ptr cinfo)
+{
+  my_error_ptr myerr = (my_error_ptr) cinfo->err;
+  (*cinfo->err->output_message) (cinfo);
+  longjmp(myerr->setjmp_buffer, 1);
+}
+
+#ifdef HAVE_JPEG
+VideoPixelFormat JpgFormat(jpeg_decompress_struct& /*info*/ )
+{
+    // TODO: Actually work this out properly.
+    return VideoFormatFromString("RGB24");
+}
+#endif
+
 TypedImage LoadJpg(const std::string& filename)
 {
+#ifdef HAVE_JPEG
+    FILE * infile = fopen(filename.c_str(), "rb");
+
+    if(infile) {
+        struct my_error_mgr jerr;
+        jerr.pub.error_exit = my_error_exit;
+    
+        struct jpeg_decompress_struct cinfo;
+        cinfo.err = jpeg_std_error(&jerr.pub);
+    
+        if (setjmp(jerr.setjmp_buffer)) {
+            // If we get here, the JPEG code has signaled an error.
+            jpeg_destroy_decompress(&cinfo);
+            fclose(infile);
+            throw std::runtime_error("Error whilst loading JPEG image, '" + filename + "'");
+        }
+    
+        jpeg_create_decompress(&cinfo);
+        jpeg_stdio_src(&cinfo, infile);
+        jpeg_read_header(&cinfo, TRUE);
+        jpeg_start_decompress(&cinfo);
+    
+        const int row_stride = cinfo.output_width * cinfo.output_components;
+        
+        TypedImage img;
+        img.Alloc(cinfo.output_width, cinfo.output_height, row_stride );
+        img.fmt = JpgFormat(cinfo);
+        
+        JSAMPARRAY row_buffer = (*cinfo.mem->alloc_sarray)
+                ((j_common_ptr) &cinfo, JPOOL_IMAGE, row_stride, 1);
+        
+        while (cinfo.output_scanline < cinfo.output_height) {
+            const int scanline = cinfo.output_scanline;
+            jpeg_read_scanlines(&cinfo, row_buffer, 1);
+            memcpy(img.ptr + scanline * img.pitch, row_buffer[0], img.pitch );
+        }
+        
+        jpeg_finish_decompress(&cinfo);
+        jpeg_destroy_decompress(&cinfo);
+        fclose(infile);  
+        return img;
+    }
+    throw std::runtime_error("Unable to load JPEG file, '" + filename + "'");
+#else   
     throw std::runtime_error("JPEG Support not enabled. Please rebuild Pangolin.");
+#endif
 }
 
 TypedImage LoadGif(const std::string& filename)
