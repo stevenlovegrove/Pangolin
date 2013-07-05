e9248a58b9fbbc832bc7ecf93c5c65a3830a1d98
Steven Lovegrove <stevenlovegrove@gmail.com>
2013-07-05 18:43:17 -0400

    Introduce PANGO_ constants for special keys.

--- include/pangolin/handler.h
+++ include/pangolin/handler.h
@@ -42,6 +42,29 @@ const int PANGO_SPECIAL = 128;
 const int PANGO_CTRL = -96;
 const int PANGO_OPTN = 132;
 
+// Special Keys (same as GLUT_ defines)
+const int PANGO_KEY_F1        = 1;
+const int PANGO_KEY_F2        = 2;
+const int PANGO_KEY_F3        = 3;
+const int PANGO_KEY_F4        = 4;
+const int PANGO_KEY_F5        = 5;
+const int PANGO_KEY_F6        = 6;
+const int PANGO_KEY_F7        = 7;
+const int PANGO_KEY_F8        = 8;
+const int PANGO_KEY_F9        = 9;
+const int PANGO_KEY_F10       = 10;
+const int PANGO_KEY_F11       = 11;
+const int PANGO_KEY_F12       = 12;
+const int PANGO_KEY_LEFT      = 100;
+const int PANGO_KEY_UP        = 101;
+const int PANGO_KEY_RIGHT     = 102;
+const int PANGO_KEY_DOWN      = 103;
+const int PANGO_KEY_PAGE_UP   = 104;
+const int PANGO_KEY_PAGE_DOWN = 105;
+const int PANGO_KEY_HOME      = 106;
+const int PANGO_KEY_END	      = 107;
+const int PANGO_KEY_INSERT	  = 108;
+
 enum MouseButton
 {
     MouseButtonLeft = 1,
