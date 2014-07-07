bdc3e15151954c81e739d490a4fd369313c15f7a
Richard Newcombe <r.a.newcombe@gmail.com>
2014-07-07 14:52:48 -0700

    added oculus find changes for linux
    Signed-off-by: Richard Newcombe <r.a.newcombe@gmail.com>

--- CMakeModules/FindOculus.cmake
+++ CMakeModules/FindOculus.cmake
@@ -4,11 +4,14 @@
 #  Oculus_INCLUDE_DIRS - the libuvc include directories
 #  Oculus_LIBRARIES - link these to use libuvc
 
+message(STATUS "Looking for Oculus")
+
 FIND_PATH(
   Oculus_INCLUDE_DIRS
   NAMES OVR.h
   PATHS
     ${CMAKE_SOURCE_DIR}/../LibOVR/Include
+    ${CMAKE_SOURCE_DIR}/../OculusSDK/LibOVR/Include
     /usr/include/LibOVR/Include
     /usr/local/include/LibOVR/Include
     /opt/local/include/LibOVR/Include
@@ -22,6 +25,7 @@ FIND_LIBRARY(
   NAMES ovr
   PATHS
     ${CMAKE_SOURCE_DIR}/../LibOVR/Lib/MacOS/Release
+    ${CMAKE_SOURCE_DIR}/../OculusSDK/LibOVR/Lib/Linux/Release/x86_64
     /usr/include/LibOVR/Lib
     /usr/local/include/LibOVR/Lib
     /opt/local/include/LibOVR/Lib
@@ -31,21 +35,35 @@ FIND_LIBRARY(
 )
 
 IF (Oculus_INCLUDE_DIRS AND Oculus_LIBRARIES)
-   SET(Oculus_FOUND TRUE)
+    IF(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+        find_library(CARBON_LIBRARIES NAMES Carbon)
+        find_library(IOKIT_LIBRARIES NAMES IOKit)
+        list(APPEND Oculus_LIBRARIES ${CARBON_LIBRARIES})
+        list(APPEND Oculus_LIBRARIES ${IOKIT_LIBRARIES})
+        message(STATUS "found Oculus!")
+        SET(Oculus_FOUND TRUE)
+    ELSEIF(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
+        FIND_PACKAGE(Xrandr)
+    IF( Xrandr_FOUND )
+        message(STATUS "OccyRift Found")
+        list(APPEND Oculus_LIBRARIES ${Xrandr_LIBRARIES} -ludev -lXrandr  )
+    message(STATUS "found Oculus!")
+        SET(Oculus_FOUND TRUE)
+    ELSE()
+        SET(Oculus_FOUND FALSE)
+    ENDIF()
+    ENDIF()
 ENDIF (Oculus_INCLUDE_DIRS AND Oculus_LIBRARIES)
 
-IF(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
-    find_library(CARBON_LIBRARIES NAMES Carbon)
-    find_library(IOKIT_LIBRARIES NAMES IOKit)
-    list(APPEND Oculus_LIBRARIES ${CARBON_LIBRARIES})
-    list(APPEND Oculus_LIBRARIES ${IOKIT_LIBRARIES})
-endif()
+
 
 IF (Oculus_FOUND)
    IF (NOT Oculus_FIND_QUIETLY)
       MESSAGE(STATUS "Found Oculus: ${Oculus_LIBRARIES}")
+      MESSAGE(STATUS "Found Oculus: ${Oculus_INCLUDE_DIRS}")
    ENDIF (NOT Oculus_FIND_QUIETLY)
 ELSE (Oculus_FOUND)
+message(STATUS "Oculus NOT found")
    IF (Oculus_FIND_REQUIRED)
       MESSAGE(FATAL_ERROR "Could not find Oculus")
    ENDIF (Oculus_FIND_REQUIRED)
--- examples/SimpleDisplay/main.cpp
+++ examples/SimpleDisplay/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-
 #include <pangolin/pangolin.h>
 
 struct CustomType
