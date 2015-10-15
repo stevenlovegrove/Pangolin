e853770dca4cecff3c1bae679a86df0a973500c5
Steven Lovegrove <stevenlovegrove@gmail.com>
2015-10-14 17:53:33 -0700

    Pleora: Print extended error description.

--- src/video/drivers/pleora.cpp
+++ src/video/drivers/pleora.cpp
@@ -284,7 +284,7 @@ bool PleoraVideo::GrabNext( unsigned char* image, bool /*wait*/ )
     // Retrieve next buffer
     PvResult lResult = lStream->RetrieveBuffer( &lBuffer, &lOperationResult, 1000 );
     if ( !lResult.IsOK() ) {
-        pango_print_warn("Pleora error: %s\n", lResult.GetCodeString().GetAscii() );
+        pango_print_warn("Pleora error: %s,\n'%s'\n", lResult.GetCodeString().GetAscii(), lResult.GetDescription().GetAscii() );
         return false;
     }
 
@@ -300,7 +300,7 @@ bool PleoraVideo::GrabNext( unsigned char* image, bool /*wait*/ )
             good = true;
         }
     } else {
-        pango_print_warn("Pleora error: %s\n", lOperationResult.GetCodeString().GetAscii() );
+        pango_print_warn("Pleora error: %s,\n'%s'\n", lResult.GetCodeString().GetAscii(), lResult.GetDescription().GetAscii() );
     }
 
     lStream->QueueBuffer( lBuffer );
@@ -317,10 +317,10 @@ bool PleoraVideo::GrabNewest( unsigned char* image, bool wait )
 
     PvResult lResult = lStream->RetrieveBuffer( &lBuffer, &lOperationResult, timeout );
     if ( !lResult.IsOK() ) {
-        pango_print_warn("Pleora error: %s\n", lResult.GetCodeString().GetAscii() );
+        pango_print_warn("Pleora error: %s,\n'%s'\n", lResult.GetCodeString().GetAscii(), lResult.GetDescription().GetAscii() );
         return false;
     }else if( !lOperationResult.IsOK() ) {
-        pango_print_warn("Pleora error: %s\n", lOperationResult.GetCodeString().GetAscii() );
+        pango_print_warn("Pleora error: %s,\n'%s'\n", lOperationResult.GetCodeString().GetAscii(), lResult.GetDescription().GetAscii() );
         lStream->QueueBuffer( lBuffer );
         return false;
     }
