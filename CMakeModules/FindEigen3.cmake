bb6290e96cad2e85c7b7eae7e8bbeebb3303fd62
Steven Lovegrove <stevenlovegrove@gmail.com>
2016-05-21 18:01:24 -0700

    FindEigen.cmake - add possible eigen path locations.

--- CMakeModules/FindEigen3.cmake
+++ CMakeModules/FindEigen3.cmake
@@ -63,6 +63,10 @@ else (EIGEN3_INCLUDE_DIR)
 
   find_path(EIGEN3_INCLUDE_DIR NAMES signature_of_eigen3_matrix_library
       PATHS
+      ../eigen
+      ../../eigen
+      /usr/local/include
+      /opt/local/include
       ${CMAKE_INSTALL_PREFIX}/include
       ${KDE4_INCLUDE_DIR}
       PATH_SUFFIXES eigen3 eigen
