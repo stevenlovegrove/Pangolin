e6981c61e8df8cc79155e563d296d98b6b794bab
Steven Lovegrove <stevenlovegrove@gmail.com>
2017-01-16 11:25:38 -0800

    OrthoTest

--- examples/HelloPangolin/main.cpp
+++ examples/HelloPangolin/main.cpp
@@ -1,24 +1,49 @@
 #include <pangolin/pangolin.h>
 
 int main( int /*argc*/, char** /*argv*/ )
-{  
-    pangolin::CreateWindowAndBind("Main",640,480);
+{
+    const int w = 512;
+    const int h = 512;
+
+    const int UI_WIDTH = 180;
+    pangolin::CreateWindowAndBind("Main",UI_WIDTH+w,h);
     glEnable(GL_DEPTH_TEST);
+
+    pangolin::CreatePanel("ui")
+        .SetBounds(0.0, 1.0, 0.0, pangolin::Attach::Pix(UI_WIDTH));
+
+    Eigen::Matrix4d orth = pangolin::ProjectionMatrixOrthographic(-2,2,-2,2,0.2,100);
+    Eigen::Matrix4d proj = pangolin::ProjectionMatrix(w,h,420,420,w/2.0,h/2.0,0.2,100);
+    Eigen::Matrix4d curr = proj;
 
     // Define Projection and initial ModelView matrix
     pangolin::OpenGlRenderState s_cam(
-        pangolin::ProjectionMatrix(640,480,420,420,320,240,0.2,100),
+        proj,
         pangolin::ModelViewLookAt(-2,2,-2, 0,0,0, pangolin::AxisY)
     );
 
     // Create Interactive View in window
     pangolin::Handler3D handler(s_cam);
     pangolin::View& d_cam = pangolin::CreateDisplay()
-            .SetBounds(0.0, 1.0, 0.0, 1.0, -640.0f/480.0f)
+            .SetBounds(0.0, 1.0, pangolin::Attach::Pix(UI_WIDTH), 1.0, -w/(double)h)
             .SetHandler(&handler);
+
+    Eigen::Matrix4d* target = &proj;
+
+    pangolin::RegisterKeyPressCallback('p', [&](){
+        target = &proj;
+    });
+    pangolin::RegisterKeyPressCallback('o', [&](){
+        target = &orth;
+    });
+
+    pangolin::Var<double> factor("ui.factor", 1.0);
 
     while( !pangolin::ShouldQuit() )
     {
+        curr = factor*proj + (1.0-factor)*orth;
+        s_cam.GetProjectionMatrix() = curr;
+
         // Clear screen and activate view to render into
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         d_cam.Activate(s_cam);
@@ -29,6 +54,6 @@ int main( int /*argc*/, char** /*argv*/ )
         // Swap frames and Process Events
         pangolin::FinishFrame();
     }
-    
+
     return 0;
 }
