062059f630c27b3fc45c0f3a628265d2c61622bb
Steven Lovegrove <stevenlovegrove@gmail.com>
2017-03-23 22:41:45 -0700

    gldraw: introduce glDrawVertices, glDrawColoredVertices, and some associated convenience methods.

--- include/pangolin/gl/glformattraits.h
+++ include/pangolin/gl/glformattraits.h
@@ -29,6 +29,10 @@
 
 #include <pangolin/gl/glplatform.h>
 
+#ifdef HAVE_EIGEN
+#  include <Eigen/Core>
+#endif
+
 namespace pangolin
 {
 
@@ -42,35 +46,14 @@ struct GlFormatTraits;
 //    static const T glmax = 0;
 //};
 
-//////////////////////////////////////////////////////////////////
-
-template<>
-struct GlFormatTraits<float>
-{
-    static const GLint glinternalformat = GL_LUMINANCE32F_ARB;
-    static const GLenum glformat = GL_LUMINANCE;
-    static const GLenum gltype = GL_FLOAT;
-};
-
-template<>
-struct GlFormatTraits<double>
-{
-    static const GLint glinternalformat = GL_LUMINANCE32F_ARB;
-    static const GLenum glformat = GL_LUMINANCE;
-    static const GLenum gltype = GL_DOUBLE;
-};
-
-//////////////////////////////////////////////////////////////////
-
 template<>
 struct GlFormatTraits<unsigned char>
 {
     static const GLint glinternalformat = GL_LUMINANCE32F_ARB;
     static const GLenum glformat = GL_LUMINANCE;
     static const GLenum gltype = GL_UNSIGNED_BYTE;
+    static const size_t components = 1;
 };
-
-//////////////////////////////////////////////////////////////////
 
 template<>
 struct GlFormatTraits<unsigned short>
@@ -78,6 +61,7 @@ struct GlFormatTraits<unsigned short>
     static const GLint glinternalformat = GL_LUMINANCE32F_ARB;
     static const GLenum glformat = GL_LUMINANCE;
     static const GLenum gltype = GL_UNSIGNED_SHORT;
+    static const size_t components = 1;
 };
 
 template<>
@@ -86,6 +70,7 @@ struct GlFormatTraits<unsigned int>
     static const GLint glinternalformat = GL_LUMINANCE32F_ARB;
     static const GLenum glformat = GL_LUMINANCE;
     static const GLenum gltype = GL_UNSIGNED_INT;
+    static const size_t components = 1;
 };
 
 template<>
@@ -94,6 +79,82 @@ struct GlFormatTraits<int>
     static const GLint glinternalformat = GL_LUMINANCE32F_ARB;
     static const GLenum glformat = GL_LUMINANCE;
     static const GLenum gltype = GL_INT;
+    static const size_t components = 1;
 };
+
+template<>
+struct GlFormatTraits<float>
+{
+    static const GLint glinternalformat = GL_LUMINANCE32F_ARB;
+    static const GLenum glformat = GL_LUMINANCE;
+    static const GLenum gltype = GL_FLOAT;
+    static const size_t components = 1;
+};
+
+template<>
+struct GlFormatTraits<double>
+{
+    static const GLint glinternalformat = GL_LUMINANCE32F_ARB;
+    static const GLenum glformat = GL_LUMINANCE;
+    static const GLenum gltype = GL_DOUBLE;
+    static const size_t components = 1;
+};
+
+
+
+#ifdef HAVE_EIGEN
+
+//////////////////////////////////////////////////////////////////
+
+template <>
+struct GlFormatTraits<Eigen::Vector2i>
+{
+    static const GLint glinternalformat = GL_RGB32F_ARB;
+    static const GLenum glformat = GL_RG;
+    static const GLenum gltype = GL_INT;
+    static const size_t components = 2;
+};
+
+//////////////////////////////////////////////////////////////////
+
+template <>
+struct GlFormatTraits<Eigen::Matrix<unsigned char,3,1>>
+{
+    static const GLint glinternalformat = GL_RGB;
+    static const GLenum glformat = GL_RGB;
+    static const GLenum gltype = GL_UNSIGNED_BYTE;
+    static const size_t components = 3;
+};
+
+template <>
+struct GlFormatTraits<Eigen::Vector3f>
+{
+    static const GLint glinternalformat = GL_RGB32F_ARB;
+    static const GLenum glformat = GL_RGB;
+    static const GLenum gltype = GL_FLOAT;
+    static const size_t components = 3;
+};
+
+//////////////////////////////////////////////////////////////////
+
+template <>
+struct GlFormatTraits<Eigen::Matrix<unsigned char,4,1>>
+{
+    static const GLint glinternalformat = GL_RGBA;
+    static const GLenum glformat = GL_RGBA;
+    static const GLenum gltype = GL_UNSIGNED_BYTE;
+    static const size_t components = 4;
+};
+
+template <>
+struct GlFormatTraits<Eigen::Vector4f>
+{
+    static const GLint glinternalformat = GL_RGBA32F_ARB;
+    static const GLenum glformat = GL_RGBA;
+    static const GLenum gltype = GL_FLOAT;
+    static const size_t components = 4;
+};
+
+#endif // HAVE_EIGEN
 
 }
