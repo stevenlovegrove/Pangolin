6598e9b73ff2961c263c40dc4805ac801dec9b21
Steven Lovegrove <stevenlovegrove@gmail.com>
2017-03-23 22:55:48 -0700

    RenderVboCbo: support different OpenGL vertex array modes.

--- include/pangolin/gl/glvbo.h
+++ include/pangolin/gl/glvbo.h
@@ -42,7 +42,7 @@ GlBuffer MakeTriangleStripIboForVbo(int w, int h);
 
 void RenderVbo(GlBuffer& vbo, GLenum mode = GL_POINTS);
 
-void RenderVboCbo(GlBuffer& vbo, GlBuffer& cbo, bool draw_color = true);
+void RenderVboCbo(GlBuffer& vbo, GlBuffer& cbo, bool draw_color = true, GLenum mode = GL_POINTS);
 
 void RenderVboIbo(GlBuffer& vbo, GlBuffer& ibo, bool draw_mesh = true);
 
@@ -103,7 +103,7 @@ inline void RenderVbo(GlBuffer& vbo, GLenum mode)
     vbo.Unbind();
 }
 
-inline void RenderVboCbo(GlBuffer& vbo, GlBuffer& cbo, bool draw_color)
+inline void RenderVboCbo(GlBuffer& vbo, GlBuffer& cbo, bool draw_color, GLenum mode )
 {
     if(draw_color) {
         cbo.Bind();
@@ -111,7 +111,7 @@ inline void RenderVboCbo(GlBuffer& vbo, GlBuffer& cbo, bool draw_color)
         glEnableClientState(GL_COLOR_ARRAY);
     }
     
-    RenderVbo(vbo);
+    RenderVbo(vbo,mode);
     
     if(draw_color) {
         glDisableClientState(GL_COLOR_ARRAY);
