bcec3480e1ba86baeb2414dd056bfd924f41a856
Steven Lovegrove <stevenlovegrove@fb.com>
2017-03-27 15:52:27 -0700

    Revert "Factor VideoViewer into reusable class, courtesy of Svet."
    This reverts commit b01d364e5e897217781968c7998ec360745396ef.

--- include/pangolin/tools/video_viewer.h
+++ include/pangolin/tools/video_viewer.h
@@ -1,50 +1,9 @@
 #include <pangolin/platform.h>
-#include <pangolin/video/video_input.h>
-#include <pangolin/var/var.h>
-
-#include <mutex>
-#include <thread>
 #include <string>
-#include <atomic>
 
 namespace pangolin
 {
 
-
-class VideoViewer
-{
-public:
-    
-    VideoViewer(const std::string& input_uri, const std::string& output_uri, const bool new_thread=false);
-    ~VideoViewer();
-    
-    void StartRecording();
-    void StopRecording();
-    void RecordOneFrame();
-    void ChangeOutputURL(const std::string& url);
-    
-    pangolin::Var<bool>& VideoWait();
-    pangolin::Var<bool>& VideoNewest();
-    
-private:
-    
-    void RunUI();
-    
-    VideoInput video_;
-    
-    pangolin::Var<bool> video_wait_;
-    pangolin::Var<bool> video_newest_;
-    
-    
-    std::mutex mutex;
-    std::unique_ptr<std::thread> thread_;
-    std::atomic<bool> toquit_;
-    
-    
-};
-
-
-
-void RunVideoViewerUI(const std::string& input_uri, const std::string& output_uri);
+void PANGOLIN_EXPORT RunVideoViewerUI(const std::string& input_uri, const std::string& output_uri);
 
 }
