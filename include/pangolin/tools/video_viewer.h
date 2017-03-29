df5a626d369abc9448859a4facae815551187e8a
Steven Lovegrove <stevenlovegrove@gmail.com>
2017-03-28 20:53:43 -0700

    Add VideoViewer reusable object.

--- include/pangolin/tools/video_viewer.h
+++ include/pangolin/tools/video_viewer.h
@@ -1,8 +1,71 @@
 #include <pangolin/platform.h>
+#include <pangolin/display/window.h>
+#include <pangolin/video/video_input.h>
+
+#include <mutex>
+#include <thread>
 #include <string>
 
 namespace pangolin
 {
+
+PANGOLIN_EXPORT
+class VideoViewer
+{
+public:
+    static constexpr int FRAME_SKIP = 30;
+
+    VideoViewer(const std::string& window_name, const std::string& input_uri, const std::string& output_uri = "video.pango" );
+    VideoViewer(const VideoViewer&) = delete;
+
+    ~VideoViewer();
+
+    void Quit();
+
+    inline int TotalFrames() const
+    {
+        return video_playback ? video_playback->GetTotalFrames() : std::numeric_limits<int>::max();
+    }
+
+    // Control playback
+    void OpenInput(const std::string& input_uri);
+    void CloseInput();
+
+    // Control recording
+    void Record();
+    void RecordOneFrame();
+    void StopRecording();
+
+    // Useful for user-control
+    void TogglePlay();
+    void ToggleRecord();
+    void ToggleDiscardBufferedFrames();
+    void ToggleWaitForFrames();
+    void Skip(int frames);
+
+    void WaitUntilExit();
+
+protected:
+    void RegisterDefaultKeyShortcutsAndPangoVariables();
+    void Run();
+
+    std::mutex control_mutex;
+    std::string window_name;
+    std::thread vv_thread;
+
+    pangolin::VideoInput video;
+    pangolin::VideoPlaybackInterface* video_playback;
+
+    std::string output_uri;
+
+    int current_frame;
+    int grab_until;
+    int record_nth_frame;
+    bool video_grab_wait;
+    bool video_grab_newest;
+    bool should_run;
+};
+
 
 void PANGOLIN_EXPORT RunVideoViewerUI(const std::string& input_uri, const std::string& output_uri);
 
--- src/tools/video_viewer.cpp
+++ src/tools/video_viewer.cpp
@@ -19,73 +19,63 @@ void videoviewer_signal_quit(int) {
     pangolin::QuitAll();
 }
 
-PANGOLIN_EXPORT
-void RunVideoViewerUI(const std::string& input_uri, const std::string& output_uri)
+VideoViewer::VideoViewer(const std::string& window_name, const std::string& input_uri, const std::string& output_uri)
+    : window_name(window_name),
+      video_playback(nullptr),
+      output_uri(output_uri),
+      current_frame(-1),
+      grab_until(std::numeric_limits<int>::max()),
+      record_nth_frame(1),
+      video_grab_wait(true),
+      video_grab_newest(false),
+      should_run(true)
 {
-    RegisterNewSigCallback(videoviewer_signal_quit, nullptr, SIGINT);
-    RegisterNewSigCallback(videoviewer_signal_quit, nullptr, SIGTERM);
+    pangolin::Var<int>::Attach("ui.frame", current_frame);
 
+    if(!input_uri.empty()) {
+        OpenInput(input_uri);
+    }
+
+    vv_thread = std::thread(&VideoViewer::Run, this);
+}
+
+VideoViewer::~VideoViewer()
+{
+    Quit();
+
+    // Wait for programming to close.
+    if(vv_thread.joinable()) {
+        vv_thread.join();
+    }
+}
+
+void VideoViewer::Quit()
+{
+    should_run = false;
+}
+
+void VideoViewer::Run()
+{
     /////////////////////////////////////////////////////////////////////////
     /// Register pangolin variables
     /////////////////////////////////////////////////////////////////////////
 
-    pangolin::Var<int>  record_timelapse_frame_skip("viewer.record_timelapse_frame_skip", 1 );
-    pangolin::Var<int>  end_frame("viewer.end_frame", std::numeric_limits<int>::max() );
-    pangolin::Var<bool> video_wait("video.wait", true);
-    pangolin::Var<bool> video_newest("video.newest", false);
-
-    /////////////////////////////////////////////////////////////////////////
-    /// Setup Video streams
-    /////////////////////////////////////////////////////////////////////////
-
-    // Open Video by URI
-    pangolin::VideoInput video(input_uri, output_uri);
-    const size_t num_streams = video.Streams().size();
-
-    if(num_streams == 0) {
-        pango_print_error("No video streams from device.\n");
-        return;
-    }
-
-    // Output details of video stream
-    for(size_t s = 0; s < num_streams; ++s) {
-        const pangolin::StreamInfo& si = video.Streams()[s];
-        std::cout << "Stream " << s << ": " << si.Width() << " x " << si.Height()
-                  << " " << si.PixFormat().format << " (pitch: " << si.Pitch() << " bytes)" << std::endl;
-    }
-
-    // Check if video supports VideoPlaybackInterface
-    pangolin::VideoPlaybackInterface* video_playback = pangolin::FindFirstMatchingVideoInterface<pangolin::VideoPlaybackInterface>(video);
-    const int total_frames = video_playback ? video_playback->GetTotalFrames() : std::numeric_limits<int>::max();
-    const int slider_size = (total_frames < std::numeric_limits<int>::max() ? 20 : 0);
-
-    if( video_playback ) {
-        if(total_frames < std::numeric_limits<int>::max() ) {
-            std::cout << "Video length: " << total_frames << " frames" << std::endl;
-        }
-        end_frame = 0;
-    }
-
     std::unique_ptr<unsigned char[]> buffer(new unsigned char[video.SizeBytes()+1]);
 
-    /////////////////////////////////////////////////////////////////////////
-    /// Create GUI
-    /////////////////////////////////////////////////////////////////////////
-
+    const int slider_size = (TotalFrames() < std::numeric_limits<int>::max() ? 20 : 0);
 
     // Create OpenGL window - guess sensible dimensions
-    pangolin::CreateWindowAndBind( "VideoViewer",
-        (int)(video.Width() * num_streams),
+    pangolin::CreateWindowAndBind( window_name,
+        (int)(video.Width() * video.Streams().size()),
         (int)(video.Height() + slider_size)
     );
-
     glEnable (GL_BLEND);
     glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-    pangolin::Var<int> frame("ui.frame", -1, 0, total_frames-1 );
+    pangolin::Var<int> frame("ui.frame");
     pangolin::Slider frame_slider("frame", frame.Ref() );
 
-    if(video_playback && total_frames < std::numeric_limits<int>::max())
+    if(video_playback && TotalFrames() < std::numeric_limits<int>::max())
     {
         // frame_slider should be added first so that it can be rendered correctly.
         pangolin::DisplayBase().AddDisplay(frame_slider);
@@ -101,14 +91,23 @@ void RunVideoViewerUI(const std::string& input_uri, const std::string& output_ur
         container.AddDisplay(sv);
     }
 
+    pangolin::View& record_graphic = pangolin::Display("record_glyth").
+            SetBounds(pangolin::Attach::Pix(-28),1.0f, pangolin::Attach::Pix(-28), 1.0f);
+    record_graphic.extern_draw_function = [&](pangolin::View& v){
+        if(video.IsRecording()) {
+            v.ActivatePixelOrthographic();
+            pangolin::glRecordGraphic(14.0f, 14.0f, 7.0f);
+        }
+    };
+
     std::vector<pangolin::Image<unsigned char> > images;
 
     /////////////////////////////////////////////////////////////////////////
     /// Register key shortcuts
     /////////////////////////////////////////////////////////////////////////
 
+    RegisterDefaultKeyShortcutsAndPangoVariables();
 
-    const int FRAME_SKIP = 30;
     const char show_hide_keys[]  = {'1','2','3','4','5','6','7','8','9'};
     const char screenshot_keys[] = {'!','@','#','$','%','^','&','*','('};
 
@@ -117,7 +116,7 @@ void RunVideoViewerUI(const std::string& input_uri, const std::string& output_ur
         pangolin::RegisterKeyPressCallback(show_hide_keys[v], [v,&container](){
             container[v].ToggleShow();
         } );
-        pangolin::RegisterKeyPressCallback(screenshot_keys[v], [v,&images,&video](){
+        pangolin::RegisterKeyPressCallback(screenshot_keys[v], [this,v,&images](){
             if(v < images.size() && images[v].ptr) {
                 try{
                     pangolin::SaveImage(
@@ -131,101 +130,25 @@ void RunVideoViewerUI(const std::string& input_uri, const std::string& output_ur
         } );
     }
 
-    pangolin::RegisterKeyPressCallback('r', [&](){
-        if(!video.IsRecording()) {
-            video.SetTimelapse( static_cast<size_t>(record_timelapse_frame_skip) );
-            video.Record();
-            pango_print_info("Started Recording.\n");
-        }else{
-            video.Stop();
-            pango_print_info("Finished recording.\n");
-        }
-        fflush(stdout);
-    });
-    pangolin::RegisterKeyPressCallback('p', [&](){
-        video.Play();
-        end_frame = std::numeric_limits<int>::max();
-        pango_print_info("Playing from file log.\n");
-        fflush(stdout);
-    });
-    pangolin::RegisterKeyPressCallback('s', [&](){
-        video.Source();
-        end_frame = std::numeric_limits<int>::max();
-        pango_print_info("Playing from source input.\n");
-        fflush(stdout);
-    });
-    pangolin::RegisterKeyPressCallback(' ', [&](){
-        end_frame = (frame < end_frame) ? frame : std::numeric_limits<int>::max();
-    });
-    pangolin::RegisterKeyPressCallback('w', [&](){
-        video_wait = !video_wait;
-        if(video_wait) {
-            pango_print_info("Gui wait's for video frame.\n");
-        }else{
-            pango_print_info("Gui doesn't wait for video frame.\n");
-        }
-    });
-    pangolin::RegisterKeyPressCallback('d', [&](){
-        video_newest = !video_newest;
-        if(video_newest) {
-            pango_print_info("Discarding old frames.\n");
-        }else{
-            pango_print_info("Not discarding old frames.\n");
-        }
-    });
-    pangolin::RegisterKeyPressCallback('<', [&](){
-        if(video_playback) {
-            frame = video_playback->Seek(frame - FRAME_SKIP) -1;
-            end_frame = frame + 1;
-        }else{
-            pango_print_warn("Unable to skip backward.");
-        }
-    });
-    pangolin::RegisterKeyPressCallback('>', [&](){
-        if(video_playback) {
-            frame = video_playback->Seek(frame + FRAME_SKIP) -1;
-            end_frame = frame + 1;
-        }else{
-            end_frame = frame + FRAME_SKIP;
-        }
-    });
-    pangolin::RegisterKeyPressCallback(',', [&](){
-        if(video_playback) {
-            frame = video_playback->Seek(frame - 1) -1;
-            end_frame = frame+1;
-        }else{
-            pango_print_warn("Unable to skip backward.");
-        }
-    });
-    pangolin::RegisterKeyPressCallback('.', [&](){
-        // Pause at next frame
-        end_frame = frame+1;
-    });
-    pangolin::RegisterKeyPressCallback('0', [&](){
-        video.RecordOneFrame();
-    });
-
-    /////////////////////////////////////////////////////////////////////////
-    /// Main GUI loop
-    /////////////////////////////////////////////////////////////////////////
-
     video.Start();
 
     // Stream and display video
-    while(!pangolin::ShouldQuit())
+    while(should_run && !pangolin::ShouldQuit())
     {
         glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
         glColor3f(1.0f, 1.0f, 1.0f);
 
-        if(frame.GuiChanged()) {
-            if(video_playback) {
-                frame = video_playback->Seek(frame) -1;
-            }
-            end_frame = frame + 1;
-        }
+        {
+            std::lock_guard<std::mutex> lock(control_mutex);
 
-        if ( frame < end_frame ) {
-            if( video.Grab(&buffer[0], images, video_wait, video_newest) ) {
+            if(frame.GuiChanged()) {
+                if(video_playback) {
+                    frame = video_playback->Seek(frame) -1;
+                }
+                grab_until = frame + 1;
+            }
+
+            if ( frame < grab_until && video.Grab(&buffer[0], images, video_grab_wait, video_grab_newest)) {
                 frame = frame +1;
 
                 // Update images
@@ -237,11 +160,155 @@ void RunVideoViewerUI(const std::string& input_uri, const std::string& output_ur
 
         // leave in pixel orthographic for slider to render.
         pangolin::DisplayBase().ActivatePixelOrthographic();
-        if(video.IsRecording()) {
-            pangolin::glRecordGraphic(pangolin::DisplayBase().v.w-14.0f, pangolin::DisplayBase().v.h-14.0f, 7.0f);
-        }
         pangolin::FinishFrame();
     }
+
+    pangolin::DestroyWindow(window_name);
+}
+
+void VideoViewer::RegisterDefaultKeyShortcutsAndPangoVariables()
+{
+    pangolin::RegisterKeyPressCallback(' ', [this](){TogglePlay();} );
+    pangolin::RegisterKeyPressCallback('r', [this](){ToggleRecord();});
+    pangolin::RegisterKeyPressCallback('w', [this](){ToggleWaitForFrames();} );
+    pangolin::RegisterKeyPressCallback('d', [this](){ToggleDiscardBufferedFrames();} );
+    pangolin::RegisterKeyPressCallback(',', [this](){Skip(-1);} );
+    pangolin::RegisterKeyPressCallback('.', [this](){Skip(+1);} );
+    pangolin::RegisterKeyPressCallback('<', [this](){Skip(-FRAME_SKIP);} );
+    pangolin::RegisterKeyPressCallback('>', [this](){Skip(+FRAME_SKIP);} );
+    pangolin::RegisterKeyPressCallback('0', [this](){RecordOneFrame();} );
+}
+
+void VideoViewer::OpenInput(const std::string& input_uri)
+{
+    std::lock_guard<std::mutex> lock(control_mutex);
+    video.Open(input_uri, output_uri);
+
+    // Output details of video stream
+    for(size_t s = 0; s < video.Streams().size(); ++s) {
+        const pangolin::StreamInfo& si = video.Streams()[s];
+        std::cout << FormatString(
+            "Stream %: % x % % (pitch: % bytes",
+            s, si.Width(), si.Height(), si.PixFormat().format, si.Pitch()
+        ) << std::endl;
+    }
+
+    if(video.Streams().size() == 0) {
+        pango_print_error("No video streams from device.\n");
+        return;
+    }
+
+    video_playback = pangolin::FindFirstMatchingVideoInterface<pangolin::VideoPlaybackInterface>(video);
+    if(TotalFrames() < std::numeric_limits<int>::max() ) {
+        std::cout << "Video length: " << TotalFrames() << " frames" << std::endl;
+        grab_until = 0;
+    }
+
+    pangolin::Var<int> frame("ui.frame");
+    frame.Meta().range[0] = 0;
+    frame.Meta().range[1] = TotalFrames()-1;
+}
+
+void VideoViewer::CloseInput()
+{
+    std::lock_guard<std::mutex> lock(control_mutex);
+    video.Close();
+}
+
+void VideoViewer::Record()
+{
+    std::lock_guard<std::mutex> lock(control_mutex);
+    video.Record();
+}
+
+void VideoViewer::RecordOneFrame()
+{
+    std::lock_guard<std::mutex> lock(control_mutex);
+    video.RecordOneFrame();
+}
+
+void VideoViewer::StopRecording()
+{
+    std::lock_guard<std::mutex> lock(control_mutex);
+    if(video.IsRecording()) {
+        video.Stop();
+    }
+}
+
+void VideoViewer::TogglePlay()
+{
+    std::lock_guard<std::mutex> lock(control_mutex);
+    grab_until = (current_frame < grab_until) ? current_frame: std::numeric_limits<int>::max();
+}
+
+void VideoViewer::ToggleRecord()
+{
+    std::lock_guard<std::mutex> lock(control_mutex);
+    if(!video.IsRecording()) {
+        video.SetTimelapse( static_cast<size_t>(record_nth_frame) );
+        video.Record();
+        pango_print_info("Started Recording.\n");
+    }else{
+        video.Stop();
+        pango_print_info("Finished recording.\n");
+    }
+    fflush(stdout);
+}
+
+void VideoViewer::ToggleDiscardBufferedFrames()
+{
+    std::lock_guard<std::mutex> lock(control_mutex);
+    video_grab_newest = !video_grab_newest;
+    if(video_grab_newest) {
+        pango_print_info("Discarding old frames.\n");
+    }else{
+        pango_print_info("Not discarding old frames.\n");
+    }
+}
+
+void VideoViewer::ToggleWaitForFrames()
+{
+    std::lock_guard<std::mutex> lock(control_mutex);
+    video_grab_wait = !video_grab_wait;
+    if(video_grab_wait) {
+        pango_print_info("Gui wait's for video frame.\n");
+    }else{
+        pango_print_info("Gui doesn't wait for video frame.\n");
+    }
+}
+
+void VideoViewer::Skip(int frames)
+{
+    std::lock_guard<std::mutex> lock(control_mutex);
+
+    if(video_playback) {
+        current_frame = video_playback->Seek(current_frame + frames) -1;
+        grab_until = current_frame + 1;
+    }else{
+        if(frames >= 0) {
+            grab_until = current_frame + frames;
+        }else{
+            pango_print_warn("Unable to skip backward.");
+        }
+    }
+
+}
+
+void VideoViewer::WaitUntilExit()
+{
+    if(vv_thread.joinable()) {
+        vv_thread.join();
+    }
+}
+
+
+void RunVideoViewerUI(const std::string& input_uri, const std::string& output_uri)
+{
+    RegisterNewSigCallback(videoviewer_signal_quit, nullptr, SIGINT);
+    RegisterNewSigCallback(videoviewer_signal_quit, nullptr, SIGTERM);
+
+    VideoViewer vv("VideoViewer", input_uri, output_uri);
+    vv.WaitUntilExit();
 }
 
 }
