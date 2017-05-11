25e454b7af4dd342564be216beb16470b04e137b
Hauke Strasdat <hauke.strasdat@oculus.com>
2017-05-10 17:47:24 -0700

    record_nth_frame

--- src/tools/video_viewer.cpp
+++ src/tools/video_viewer.cpp
@@ -33,6 +33,8 @@ VideoViewer::VideoViewer(const std::string& window_name, const std::string& inpu
       active_cam(0)
 {
     pangolin::Var<int>::Attach("ui.frame", current_frame);
+    pangolin::Var<int>::Attach("ui.record_nth_frame", record_nth_frame);
+
 
     if(!input_uri.empty()) {
         OpenInput(input_uri);
