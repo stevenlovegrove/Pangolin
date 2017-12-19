6f45f8fd4393059bd7b6c6606cc9c929c5da6199
Hauke Strasdat <hauke.strasdat@oculus.com>
2017-12-19 11:45:39 -0800

    TeliSDK print sn on error

--- src/video/drivers/teli.cpp
+++ src/video/drivers/teli.cpp
@@ -272,7 +272,7 @@ TeliVideo::TeliVideo(const Params& p)
         );
     }
     if (uiStatus != Teli::CAM_API_STS_SUCCESS)
-        throw pangolin::VideoException("TeliSDK: Error opening camera");
+        throw pangolin::VideoException(FormatString("TeliSDK: Error opening camera, sn='%'", sn));
 
     SetDeviceParams(device_params);
     Initialise();
