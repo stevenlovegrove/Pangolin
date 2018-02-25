026477d0e1923849ddec705e8d1c40957e8e0ff6
Steven Lovegrove <steven.lovegrove@oculus.com>
2018-02-25 15:05:21 -0800

    Add Pangolin file header to new files. Use pragma once instead of include guards.

--- src/python/pypangolin/main.cpp
+++ src/python/pypangolin/main.cpp
@@ -1,6 +1,29 @@
-//
-// Copyright (c) Andrey Mnatsakanov
-//
+/* This file is part of the Pangolin Project.
+ * http://github.com/stevenlovegrove/Pangolin
+ *
+ * Copyright (c) Andrey Mnatsakanov, Steven Lovegrove
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
 
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
@@ -26,7 +49,7 @@
 
 
 PYBIND11_MODULE(pypangolin, m) {
-  m.doc() = "pybind11 example plugin"; // optional module docstring
+  m.doc() = "pypangolin python wrapper for Pangolin rapid prototyping graphics and video library.";
 
   py_pangolin::bind_var(m);
   py_pangolin::bind_viewport(m);
