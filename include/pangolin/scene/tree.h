ddaa45b3b60cc7cc4712dd6114826ffb85d7fba3
Steven Lovegrove <stevenlovegrove@gmail.com>
2018-11-03 17:16:03 -0700

    ModelViewer: Add simple render tree, spin animation and multi-threaded loading.

--- include/pangolin/scene/tree.h
+++ include/pangolin/scene/tree.h
@@ -1,24 +1,49 @@
 #pragma once
 
 #include <map>
-#include <pangolin/compat/optional.h>
+#include <pangolin/display/opengl_render_state.h>
 
 namespace pangolin {
 
-template<typename T, typename TEdgeKey, typename TEdge>
+template<typename T, typename TEdge>
 struct TreeNode
 {
+    struct Edge
+    {
+        TEdge parent_child;
+        TreeNode node;
+    };
+
     T item;
-    std::map<TEdgeKey,TEdge> edges;
+    std::vector<Edge> edges;
 };
 
-struct Renderable
-{
-    virtual ~Renderable() = 0;
-    virtual void PreRender() = 0;
-    virtual void DoRender() = 0;
-    virtual void PostRender() = 0;
-};
+template<typename T, typename TEdge>
+using NodeFunction = std::function<void(TreeNode<T,TEdge>&,const TEdge&)>;
+
+//template<typename T, typename TEdge>
+//void VisitDepthFirst(TreeNode<T,TEdge>& node, const NodeFunction<T,TEdge>& func, const TEdge& T_root_node = TEdge())
+//{
+//    func(node, T_root_node);
+//    for(auto& e : node.edges) {
+//        const TEdge T_root_child = T_root_node * e.parent_child;
+//        VisitDepthFirst(e.node, func, T_root_child);
+//    }
+//}
+
+//void Eg()
+//{
+//    using RenderNode = TreeNode<std::shared_ptr<Renderable>,OpenGlMatrix>;
+
+//    RenderNode root;
+//    VisitDepthFirst<std::shared_ptr<Renderable>,OpenGlMatrix>(
+//        root, [](RenderNode& node, const OpenGlMatrix& T_root_node) {
+//        if(node.item) {
+//            node.item->DoRender();
+//        }
+//    }, IdentityMatrix());
+
+//}
 
 
 }
