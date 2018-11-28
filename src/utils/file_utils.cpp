6a1ee20a85126b0da73b71465a9985ed2c452f9a
Scott Ramsby <scramsby@outlook.com>
2018-11-28 13:44:10 -0800

    Fix build warning if WIN32_LEAN_AND_MEAN is specified on compiler command-line

--- src/utils/file_utils.cpp
+++ src/utils/file_utils.cpp
@@ -30,7 +30,9 @@
 #include <pangolin/utils/file_utils.h>
 
 #ifdef _WIN_
-#  define WIN32_LEAN_AND_MEAN
+#  ifndef WIN32_LEAN_AND_MEAN
+#    define WIN32_LEAN_AND_MEAN
+#  endif
 #  include <Windows.h>
 #  include <Shlobj.h>
 #else
