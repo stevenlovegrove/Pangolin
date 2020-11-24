aa2ef0bc8811f80d0e294a81226b9ae1279f427b
Steven Lovegrove <stevenlovegrove@gmail.com>
2020-11-23 21:05:05 -0800

    Try github actions

--- scripts/install_prerequisites.sh
+++ scripts/install_prerequisites.sh
@@ -8,6 +8,7 @@ MANAGER=""
 VERBOSE=0
 DRYRUN=0
 UPDATE=0
+SUDO=""
 
 PKGS_UPDATE=""
 PKGS_REQUIRED=()
@@ -80,23 +81,25 @@ if ((VERBOSE > 0)); then echo "Using \"$MANAGER\" package manager (select anothe
 
 # Setup prereq commands and packages.
 if [[ "$MANAGER" == "apt-get" ]]; then
+    SUDO="sudo"
     PKGS_UPDATE="apt-get -qq update"
     PKGS_OPTIONS+=(install --no-install-suggests --no-install-recommends)
     if ((DRYRUN > 0));  then PKGS_OPTIONS+=(--dry-run); fi
     if ((VERBOSE = 0)); then PKGS_OPTIONS+=(--qq); fi
     PKGS_REQUIRED+=(libgl1-mesa-dev libwayland-dev libxkbcommon-dev wayland-protocols libegl1-mesa-dev)
-    PKGS_REQUIRED+=(libc++-dev libglew-dev libeigen3-dev libjpeg-turbo-dev libpng-dev cmake)
+    PKGS_REQUIRED+=(libc++-dev libglew-dev libeigen3-dev libjpeg-dev libpng-dev cmake)
 elif [[ "$MANAGER" == "port" ]]; then
+    SUDO="sudo"
     PKGS_UPDATE="port selfupdate -q"
     PKGS_OPTIONS+=(-N install -q)
     if ((DRYRUN > 0));  then PKGS_OPTIONS+=(-y); fi
     if ((VERBOSE = 0)); then PKGS_OPTIONS+=(--q); fi
-    PKGS_REQUIRED+=(glew eigen3-devel libjpeg-turbo libpng cmake)
+    PKGS_REQUIRED+=(glew eigen3-devel jpeg libpng cmake)
 elif [[ "$MANAGER" == "brew" ]]; then
     PKGS_UPDATE="brew update"
     PKGS_OPTIONS+=(install)
     if ((VERBOSE > 0)); then PKGS_OPTIONS+=(--verbose); fi
-    PKGS_REQUIRED+=(glew eigen jpeg-turbo libpng cmake)
+    PKGS_REQUIRED+=(glew eigen libjpeg libpng cmake)
 else
     echo "Error: Don't know how to use \"$MANAGER\", please fix the script." >&2
     exit 1
@@ -107,4 +110,4 @@ if ((UPDATE > 0)); then
 fi
 
 # Install
-$MANAGER ${PKGS_OPTIONS[*]} ${PKGS_REQUIRED[*]}
+$SUDO $MANAGER ${PKGS_OPTIONS[*]} ${PKGS_REQUIRED[*]}
