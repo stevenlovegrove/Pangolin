df7f0ec2dffb6da0c7909973c4edffb362bdc247
Steven Lovegrove <stevenlovegrove@gmail.com>
2020-12-02 13:31:40 -0800

    Get python interpreter registered again. Use static initializer trick since we don't want a compile-time link from pango_display to pango_python. This seems to work on osx, linux. A user can trigger loading manually with the generated pangolin/factory/RegisterFactoriesInterpreterInterface.h header

--- components/pango_python/src/pyinterpreter.cpp
+++ components/pango_python/src/pyinterpreter.cpp
@@ -238,7 +238,7 @@ bool PyInterpreter::PullLine(InterpreterLine& line)
     }
 }
 
-PANGOLIN_REGISTER_FACTORY(PyInterpreter)
+PANGOLIN_REGISTER_FACTORY_WITH_STATIC_INITIALIZER(PyInterpreter)
 {
     struct PyInterpreterFactory final : public TypedFactoryInterface<InterpreterInterface> {
         std::map<std::string,Precedence> Schemes() const override
