eb490d6d45fa7ba65ef1808057720e8a89b3e15b
Steven Lovegrove <stevenlovegrove@gmail.com>
2021-08-29 13:41:11 -0700

    Fixed dodgey font embedding to be rule based. Exposes factory file bug.

--- cmake/EmbedBinaryFiles.cmake
+++ cmake/EmbedBinaryFiles.cmake
@@ -1,23 +1,10 @@
-# Creates C resources file from files in given directory
+# Creates C resources file from specified files
 # Based on http://stackoverflow.com/a/27206982
-function(embed_binary_files file_glob output)
-    # Collect input files
-    file(GLOB bins ${file_glob})
-    # Stop when output file is newer than all binary files
-    set(output_newer_than_bins 1)
-    foreach(bin ${bins})
-        if(bin IS_NEWER_THAN output)
-            set(output_newer_than_bins 0)
-            break()
-        endif()
-    endforeach()
-    if(output_newer_than_bins)
-        return()
-    endif()
+function(embed_binary_files_now output binary_files)
     # Create empty output file
     file(WRITE ${output} "")
     # Iterate through input files
-    foreach(bin ${bins})
+    foreach(bin ${binary_files})
         # Get short filename
         string(REGEX MATCH "([^/]+)$" filename ${bin})
         # Replace filename spaces & extension separator for C compatibility
@@ -30,3 +17,21 @@ function(embed_binary_files file_glob output)
         file(APPEND ${output} "extern const unsigned char ${filename}[] = {${filedata}};\nextern const unsigned ${filename}_size = sizeof(${filename});\n")
     endforeach()
 endfunction()
+
+# Sets up rule for embedding files when they are newer than the output file (or it doesn't exist yet)
+function(embed_binary_files_rule output binary_files)
+    add_custom_command(
+        OUTPUT ${output}
+        DEPENDS ${binary_files}
+        COMMAND
+            ${CMAKE_COMMAND} -DINPUT_BINARY_FILES=${binary_files}
+                             -DOUTPUT_SRC_FILE=${output}
+                             -P ${PROJECT_SOURCE_DIR}/cmake/EmbedBinaryFiles.cmake
+        COMMENT "Embedding ${binary_files} into ${output}"
+    )
+endfunction()
+
+if(CMAKE_SCRIPT_MODE_FILE AND NOT CMAKE_PARENT_LIST_FILE)
+    # Running in script mode as part of build-time procedure to actually to the embedding
+    embed_binary_files_now(${OUTPUT_SRC_FILE} ${INPUT_BINARY_FILES} )
+endif()
