47e5f829d0a8f31072c2fd950734dbebd85c1a32
Steven Lovegrove <stevenlovegrove@gmail.com>
2021-09-03 11:10:01 -0700

    Example: Simplify HelloPangolinOffscreen example. Fix rendering for scaled windows.

--- examples/HelloPangolinOffscreen/main.cpp
+++ examples/HelloPangolinOffscreen/main.cpp
@@ -17,35 +17,28 @@ int main( int /*argc*/, char** /*argv*/ )
         pangolin::ModelViewLookAt(-2,2,-2, 0,0,0, pangolin::AxisY)
     );
 
-    // Create Interactive View in window
-    pangolin::Handler3D handler(s_cam);
-    pangolin::View& d_cam = pangolin::CreateDisplay()
-            .SetBounds(0.0, 1.0, 0.0, 1.0, -float(w)/float(h))
-            .SetHandler(&handler);
-
-    pangolin::SaveWindowOnRender("window");
-
     // create a frame buffer object with colour and depth buffer
     pangolin::GlTexture color_buffer(w,h);
     pangolin::GlRenderBuffer depth_buffer(w,h);
     pangolin::GlFramebuffer fbo_buffer(color_buffer, depth_buffer);
+
     fbo_buffer.Bind();
+    {
+        // Clear screen and activate view to render into
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glViewport(0,0,w,h);
+        s_cam.Apply();
 
-    // Clear screen and activate view to render into
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    d_cam.Activate(s_cam);
+        // Render OpenGL Cube
+        pangolin::glDrawColouredCube();
 
-    // Render OpenGL Cube
-    pangolin::glDrawColouredCube();
-
-    // Swap frames and Process Events
-    pangolin::FinishFrame();
-
+        // Swap frames and Process Events
+        pangolin::FinishFrame();
+    }
     fbo_buffer.Unbind();
+
     // download and save the colour buffer
     color_buffer.Save("fbo.png", false);
 
-    pangolin::QuitAll();
-    
     return 0;
 }
