491c8312ea6a4f9235bdf442c7e9ddcad9dceae1
Steven Lovegrove <stevenlovegrove@gmail.com>
2021-09-05 14:41:11 -0700

    pango_opengl: Add some GlBufferData and GlBuffer utilities for working with std::vectors and Eigen matrices.

--- components/pango_opengl/include/pangolin/gl/gl.h
+++ components/pango_opengl/include/pangolin/gl/gl.h
@@ -185,8 +185,14 @@ struct PANGOLIN_EXPORT GlBufferData
 {
     //! Default constructor represents 'no buffer'
     GlBufferData();
-    GlBufferData(GlBufferType buffer_type, GLuint size_bytes, GLenum gluse = GL_DYNAMIC_DRAW, const unsigned char* data = 0 );
+
+    GlBufferData(GlBufferType buffer_type, GLuint size_bytes, GLenum gluse = GL_DYNAMIC_DRAW, const void *data = 0 );
+
+    template<typename T>
+    GlBufferData(GlBufferType buffer_type, const std::vector<T>& data, GLenum gluse = GL_STATIC_DRAW);
+
     virtual ~GlBufferData();
+
     void Free();
 
     //! Move Constructor
@@ -197,13 +203,22 @@ struct PANGOLIN_EXPORT GlBufferData
 
     size_t SizeBytes() const;
     
-    void Reinitialise(GlBufferType buffer_type, GLuint size_bytes, GLenum gluse = GL_DYNAMIC_DRAW, const unsigned char* data = 0 );
+    void Reinitialise(GlBufferType buffer_type, GLuint size_bytes, GLenum gluse = GL_DYNAMIC_DRAW, const void *data = 0 );
     
     void Bind() const;
     void Unbind() const;
     void Upload(const GLvoid* data, GLsizeiptr size_bytes, GLintptr offset = 0);
     void Download(GLvoid* ptr, GLsizeiptr size_bytes, GLintptr offset = 0) const;
     
+
+    template<typename T>
+    void Upload(const std::vector<T>& data, GLintptr offset = 0);
+
+#ifdef USE_EIGEN
+    template<typename Derived>
+    void Upload(const Eigen::DenseBase<Derived>& data, GLintptr offset = 0);
+#endif
+
     GLuint bo;
     GlBufferType buffer_type;
     GLenum gluse;
@@ -226,9 +241,14 @@ struct PANGOLIN_EXPORT GlBuffer : public GlBufferData
     GlBuffer(GlBuffer&& tex);
     GlBuffer& operator=(GlBuffer&& tex);
     
-    void Reinitialise(GlBufferType buffer_type, GLuint num_elements, GLenum datatype, GLuint count_per_element, GLenum gluse, const unsigned char* data = nullptr );
+    void Reinitialise(GlBufferType buffer_type, GLuint num_elements, GLenum datatype, GLuint count_per_element, GLenum gluse, const void* data = nullptr );
     void Reinitialise(GlBuffer const& other );
     void Resize(GLuint num_elements);
+
+#ifdef USE_EIGEN
+    template<typename Scalar, int R, int C>
+    GlBuffer(GlBufferType buffer_type, const std::vector<Eigen::Matrix<Scalar, R,C>>& data, GLenum gluse = GL_STATIC_DRAW);
+#endif
             
     GLenum datatype;
     GLuint num_elements;
