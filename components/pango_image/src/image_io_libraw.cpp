5dce916d1e1098ebfb2de73c86458c775cddb030
Hauke Strasdat <strasdat@gmail.com>
2022-12-05 21:03:03 -0800

    libraw; todo: fix

--- components/pango_image/src/image_io_libraw.cpp
+++ components/pango_image/src/image_io_libraw.cpp
@@ -11,34 +11,36 @@ IntensityImage<> LoadLibRaw(
     const std::string& filename
 ) {
 #ifdef HAVE_LIBRAW
-    static LibRaw RawProcessor;
+    throw std::runtime_error("implement me.");
 
-    int ret;
+    // static LibRaw RawProcessor;
 
-    if ((ret = RawProcessor.open_file(filename.c_str())) != LIBRAW_SUCCESS)
-    {
-        throw std::runtime_error(libraw_strerror(ret));
-    }
+    // int ret;
 
-    if ((ret = RawProcessor.unpack()) != LIBRAW_SUCCESS)
-    {
-        throw std::runtime_error(libraw_strerror(ret));
-    }
+    // if ((ret = RawProcessor.open_file(filename.c_str())) != LIBRAW_SUCCESS)
+    // {
+    //     throw std::runtime_error(libraw_strerror(ret));
+    // }
 
-    const auto& S = RawProcessor.imgdata.sizes;
-    TypedImage image(S.width, S.height, PixelFormatFromString("GRAY16LE"), sizeof(uint16_t) * S.raw_width);
-    PitchedCopy((char*)image.ptr, image.pitch, (char*)RawProcessor.imgdata.rawdata.raw_image, sizeof(uint16_t) * S.raw_width, sizeof(uint16_t) * image.w, image.h);
+    // if ((ret = RawProcessor.unpack()) != LIBRAW_SUCCESS)
+    // {
+    //     throw std::runtime_error(libraw_strerror(ret));
+    // }
 
-    // TODO: Support image metadata so that we can extract these fields.
-    // RawProcessor.imgdata.other.iso_speed
-    // RawProcessor.imgdata.other.aperture
-    // RawProcessor.imgdata.other.focal_len
-    // RawProcessor.imgdata.other.shutter
-    // RawProcessor.imgdata.other.timestamp
-    // RawProcessor.imgdata.makernotes.common.exifCameraElevationAngle
-    // RawProcessor.imgdata.makernotes.sony.SonyDateTime
+    // const auto& S = RawProcessor.imgdata.sizes;
+    // TypedImage image(S.width, S.height, PixelFormatFromString("GRAY16LE"), sizeof(uint16_t) * S.raw_width);
+    // PitchedCopy((char*)image.ptr, image.pitch, (char*)RawProcessor.imgdata.rawdata.raw_image, sizeof(uint16_t) * S.raw_width, sizeof(uint16_t) * image.w, image.h);
 
-    return image;
+    // // TODO: Support image metadata so that we can extract these fields.
+    // // RawProcessor.imgdata.other.iso_speed
+    // // RawProcessor.imgdata.other.aperture
+    // // RawProcessor.imgdata.other.focal_len
+    // // RawProcessor.imgdata.other.shutter
+    // // RawProcessor.imgdata.other.timestamp
+    // // RawProcessor.imgdata.makernotes.common.exifCameraElevationAngle
+    // // RawProcessor.imgdata.makernotes.sony.SonyDateTime
+
+    // return image;
 #else
     PANGOLIN_UNUSED(filename);
     throw std::runtime_error("Rebuild Pangolin for libraw support.");
