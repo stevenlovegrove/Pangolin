35c716a6f99f02bcd3d288db8fe3c3af93acfcf9
Hauke Strasdat <strasdat@gmail.com>
2022-12-21 09:30:07 -0800

    bug fix: draw group - keep it simple for now

--- components/pango_context/include/pangolin/gui/drawn_group.h
+++ components/pango_context/include/pangolin/gui/drawn_group.h
@@ -5,18 +5,12 @@
 namespace pangolin
 {
 
+// By design, ignoring the pose of the children
 struct DrawnGroup : public Drawable {
   void draw(const ViewParams& params) override
   {
-    // Assumption world_from_drawable is already in params.camera_from_world
-    // TODO: rename params.camera_from_world since it isn't the world but
-    // drawable frame.
-    ViewParams child_params = params;
-
     for (auto& child : children) {
-      child_params.camera_from_world =
-          params.camera_from_world * child->pose.worldFromDrawableMatrix();
-      child->draw(child_params);
+      child->draw(params);
     }
   }
 
