fe57db532ba2a48319f09a4f2106cc5625ee74a9
Steven Lovegrove <stevenlovegrove@gmail.com>
2022-12-30 01:16:24 -0800

    Fix fileutils.cpp missing header / lib on windows

--- components/pango_core/src/file_utils.cpp
+++ components/pango_core/src/file_utils.cpp
@@ -34,7 +34,8 @@
 #    define WIN32_LEAN_AND_MEAN
 #  endif
 #  include <Windows.h>
-#  include <Shlobj.h>
+#  include <shlobj.h>
+#  include <shlwapi.h>
 #  ifdef UNICODE
 #    include <codecvt>
 #  endif
