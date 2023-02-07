9d9e613c3ca20686d5d91c3a9a51310107dcf1ea
Hauke Strasdat <strasdat@gmail.com>
2023-02-07 14:55:37 -0800

    fix: fixed aspect ratio reduction bug
    also, some layout refactor to make preconditions clearer

--- components/pango_context/src/layer_group_test.cpp
+++ components/pango_context/src/layer_group_test.cpp
@@ -1,1 +1,14 @@
+#include <catch2/catch_test_macros.hpp>
+#include <pangolin/gui/all_layers.h>
 #include <pangolin/gui/layer_group.h>
+
+namespace pangolin
+{
+
+TEST_CASE("computeLayoutConstraints, smoke")
+{
+  Shared<DrawLayer> default_draw_layer = pangolin::DrawLayer::Create({});
+  LayerGroup group = makeLayerGroup(default_draw_layer);
+  computeLayoutConstraints(group);
+}
+}  // namespace pangolin
