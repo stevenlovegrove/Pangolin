8fd7cb0444bb24dabf7154e19f343aad8ee1cefc
Steven Lovegrove <stevenlovegrove@gmail.com>
2023-02-07 02:38:52 -0800

    Simplify DeviceBuffer API and add some features / safety checks

--- components/pango_context/include/pangolin/render/device_buffer.h
+++ components/pango_context/include/pangolin/render/device_buffer.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "shared_data_package.h"
+
 #include <pangolin/render/extra_pixel_traits.h>
 #include <pangolin/utils/scoped_bind.h>
 #include <pangolin/utils/shared.h>
@@ -8,100 +10,65 @@
 namespace pangolin
 {
 
-template <typename T>
-concept ContainerWithDataMethod = requires(T)
-{
-  (void*)std::declval<T>().data();
-};
-
-template <ContainerWithDataMethod Container>
-std::shared_ptr<void> makeTypeErasedSharedPtr(Container&& container)
-{
-  using C = std::decay_t<Container>;
-  auto shared_data = std::make_shared<C>(std::forward<Container>(container));
-  auto shared_void = std::shared_ptr<void>(shared_data, shared_data->data());
-  return shared_void;
-}
-
+// Represents a std::vector-like device buffer, with type-erasure
 struct DeviceBuffer {
-  enum class Kind {
-    VertexIndices,
-    VertexAttributes,
-    // ... add more
-  };
+  enum class Kind { VertexIndices, VertexAttributes };
 
   virtual ~DeviceBuffer(){};
 
   virtual ScopedBind<DeviceBuffer> bind() const = 0;
 
-  // Returns true if this object is uninitialized and contains
-  // no data or typed information
+  // Returns true iff size() == 0
   virtual bool empty() const = 0;
 
-  virtual sophus::RuntimePixelType dataType() const = 0;
-  virtual size_t numElements() const = 0;
+  // Return the type for the stored data, if allocated, otherwise nullopt
+  virtual std::optional<sophus::RuntimePixelType> dataType() const = 0;
 
-  struct UpdateParams {
-    size_t dest_element = 0;
-    size_t num_reserve_elements = 0;
-  };
-  struct Data {
-    sophus::RuntimePixelType data_type;
-    size_t num_elements = 0;
-    std::shared_ptr<void> data;
-    UpdateParams params = {};
-  };
+  // size in number of stored dataType() elements
+  virtual size_t size() const = 0;
 
-  /// Preconditions (panics if violated):
-  ///
-  ///   - u.data must not be null
-  ///   - u.num_elements must be >= 1.
-  virtual void pushToUpdateQueue(const Data& u) = 0;
+  // Maximum size (in elements) before reallocation would occur
+  virtual size_t capacity() const = 0;
 
-  // Use with any contiguous, movable or copyable container with a
-  // data() and size() method.
-  template <ContainerWithDataMethod Container>
-  void update(
-      Container&& data,
-      UpdateParams params = {.dest_element = 0, .num_reserve_elements = 0})
-  {
-    if (!data.data()) {
-      return;
-    }
+  // Set capacity for next allocation
+  virtual void reserve(size_t) const = 0;
 
-    using T = std::decay_t<decltype(*data.data())>;
-    const size_t num_elements = data.size();
+  // size of allocated buffer in bytes. Will be at least `capacity() *
+  // dataType().bytesPerPixel()`.
+  virtual size_t sizeBytes() const = 0;
 
-    pushToUpdateQueue({
-        .data_type = sophus::RuntimePixelType::fromTemplate<T>(),
-        .num_elements = num_elements,
-        .data = makeTypeErasedSharedPtr(data),
-        .params = params,
-    });
-  }
+  // Queue upload of data into position `dest_element` of device buffer. The
+  // data will be uploaded on the next call to `sync()`. In the future, may be
+  // pipelined asynchronously, but completion is only guarenteed at the next
+  // sync() call. Safe to call from any thread.
+  //
+  // When (dest_element + data.num_elements) extends beyond the current size(),
+  // size() is increased until capacity(). If `Params::grow_to_fit` is true, a
+  // reallocation and copy will occur, otherwise an error is thrown.
+  //
+  // If `allow_retyping` is true, data can differ from dataType() in type, but
+  // existing data will be cleared and size will be set to dest_element +
+  // data.num_elements, otherwise an error is thrown
 
+  // preconditions:
+  //  - data.data != nullptr;
+  //  - if !grow_to_fit then data.num_elements + dest_element < capacity()
+  //  - if !allow_retyping then `dataType()` != nullopt, then data.data_type
+  //  must equal `dataType()`.
+  virtual void queueUpdate(
+      const SharedDataPackage& data, size_t dest_element = 0) = 0;
+
+  // Upload pending data in device queue now (blocking).
   virtual void sync() const = 0;
 
   struct Params {
-    Kind kind;
+    Kind kind = Kind::VertexAttributes;
+    size_t min_element_size = 0;
+    bool grow_to_fit = true;
+    bool allow_retyping = true;
+    std::optional<SharedDataPackage> data = std::nullopt;
   };
   static Shared<DeviceBuffer> Create(Params p);
-
-  template <typename T>
-  static Shared<DeviceBuffer> Create(std::vector<T>& vec, Params p)
-  {
-    auto buffer = DeviceBuffer::Create(p);
-    buffer->update(vec);
-    return buffer;
-  }
-
-  template <typename T>
-  static Shared<DeviceBuffer> Create(std::vector<T>&& vec, Params p)
-  {
-    auto buffer = DeviceBuffer::Create(p);
-    buffer->update(std::move(vec));
-    return buffer;
-  }
 };
 
 }  // namespace pangolin
