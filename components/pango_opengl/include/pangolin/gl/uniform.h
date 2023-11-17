0cb6dc3c9bacda19030ff52a53a316fc13bf0450
Ian Sherman <ian.j.sherman@gmail.com>
2023-11-16 23:35:33 -0800

    Add support for more 64bit uniforms

--- components/pango_opengl/include/pangolin/gl/uniform.h
+++ components/pango_opengl/include/pangolin/gl/uniform.h
@@ -31,15 +31,19 @@ void glUniformArray(GLint location, const T* val);
 PANGO_DEF_UNIFORM_ARR(GLfloat, 1fv, 1, 1)
 PANGO_DEF_UNIFORM_ARR(GLint, 1iv, 1, 1)
 PANGO_DEF_UNIFORM_ARR(GLuint, 1uiv, 1, 1)
+PANGO_DEF_UNIFORM_ARR(GLdouble, 1dv, 1, 1)
 PANGO_DEF_UNIFORM_ARR(GLfloat, 2fv, 2, 1)
 PANGO_DEF_UNIFORM_ARR(GLint, 2iv, 2, 1)
 PANGO_DEF_UNIFORM_ARR(GLuint, 2uiv, 2, 1)
+PANGO_DEF_UNIFORM_ARR(GLdouble, 2dv, 2, 1)
 PANGO_DEF_UNIFORM_ARR(GLfloat, 3fv, 3, 1)
 PANGO_DEF_UNIFORM_ARR(GLint, 3iv, 3, 1)
 PANGO_DEF_UNIFORM_ARR(GLuint, 3uiv, 3, 1)
+PANGO_DEF_UNIFORM_ARR(GLdouble, 3dv, 3, 1)
 PANGO_DEF_UNIFORM_ARR(GLfloat, 4fv, 4, 1)
 PANGO_DEF_UNIFORM_ARR(GLint, 4iv, 4, 1)
 PANGO_DEF_UNIFORM_ARR(GLuint, 4uiv, 4, 1)
+PANGO_DEF_UNIFORM_ARR(GLdouble, 4dv, 4, 1)
 #undef PANGO_DEF_UNIFORM_ARR
 
 #define PANGO_DEF_UNIFORM_MAT_ARR(type, postfix, R, C)                         \
