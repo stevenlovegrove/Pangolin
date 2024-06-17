70c219da7cb865aadb5d3915790857bfcd5f531c
Christian Rauch <Rauch.Christian@gmx.de>
2024-06-17 20:34:27 +0200

    update Catch2 header and targets

--- components/pango_video/tests/tests_video_uri.cpp
+++ components/pango_video/tests/tests_video_uri.cpp
@@ -1,5 +1,9 @@
 #define CATCH_CONFIG_MAIN
+#if __has_include(<catch2/catch.hpp>)
 #include <catch2/catch.hpp>
+#else
+#include <catch2/catch_test_macros.hpp>
+#endif
 
 #include <string>
 #include <limits>
